ddadee8c652e795d0b9b31af362d5f50973d4045
Daniel Leal <dlleal@uc.cl>
2022-03-24 02:15:04 -0300

    chore: add some comments to the types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,15 @@
+/**
+ * The options used to create the widget. You can read more
+ * here: {@link https://docs.fintoc.com/docs/widget}.
+ */
 export type FintocWidgetOptions = Record<string, any>;
 
 export type WebViewEventName = 'fintocwidget://succeeded' | 'fintocwidget://exit';
 
+/**
+ * The event handlers used to handle the WebView redirections.
+ * You can read more here: {@link https://docs.fintoc.com/docs/widget-webview-integration#webview-redirections}.
+ */
 export interface FintocWidgetEventHandlers {
   onSuccess: () => void
   onExit: () => void
@@ -9,6 +17,9 @@ export interface FintocWidgetEventHandlers {
 
 export type WebViewEventMap = Record<WebViewEventName, keyof FintocWidgetEventHandlers>
 
+/**
+ * The props for the Fintoc Widget WebView.
+ */
 export interface FintocWidgetWebViewPropsType {
   options: FintocWidgetOptions
   handlers: FintocWidgetEventHandlers
