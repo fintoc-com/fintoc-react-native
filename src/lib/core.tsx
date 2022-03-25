7bf144442a168fa1ba3c5327d7aecfac9c048a47
Daniel Leal <dlleal@uc.cl>
2022-03-24 22:43:35 -0300

    fix: translate base urls array name

--- src/lib/core.tsx
+++ src/lib/core.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { WebView } from 'react-native-webview';
 import { View, StyleSheet } from 'react-native';
 import type { FintocWidgetWebViewPropsType } from '../types';
-import { FINTOC_WEBVIEW_URL, FINTOC_URLS } from './constants';
+import { FINTOC_WEBVIEW_URL, FINTOC_BASE_URLS } from './constants';
 import { buildQueryString, buildMessageHandler } from './utils';
 
 export const FintocWidgetWebView = ({ options, handlers }: FintocWidgetWebViewPropsType) => (
@@ -10,7 +10,7 @@ export const FintocWidgetWebView = ({ options, handlers }: FintocWidgetWebViewPr
   <View style={styles.wrapper}>
     <WebView
       source={{ uri: `${FINTOC_WEBVIEW_URL}?${buildQueryString(options)}` }}
-      originWhitelist={FINTOC_URLS}
+      originWhitelist={FINTOC_BASE_URLS}
       onMessage={buildMessageHandler(handlers)}
     />
   </View>
