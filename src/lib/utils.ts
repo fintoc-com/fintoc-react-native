b8ac266a5b72f861684b2c5abd972cfbb38aa9c0
Daniel Leal <dlleal@uc.cl>
2022-03-31 09:38:06 -0300

    fix: use a default handler if one is not provided

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,11 +2,13 @@ import { WebViewMessageEvent } from 'react-native-webview';
 import type { FintocWidgetEventHandlers, WebViewEventName } from '../types';
 import { EVENT_MAP } from './constants';
 
+const defaultHanlder = () => null;
+
 export const buildQueryString = (config: Record<string, string>): string => Object.keys(config).map((key) => `${key}=${config[key]}`).join('&');
 
 export const buildMessageHandler = (handlers: FintocWidgetEventHandlers) => (
   (event: WebViewMessageEvent) => {
     const eventName = event.nativeEvent.data as WebViewEventName;
-    return handlers[EVENT_MAP[eventName]]();
+    return (handlers[EVENT_MAP[eventName]] || defaultHanlder)();
   }
 );
